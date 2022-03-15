c4165251aa415cfcbaf52301ab0f0e862c7c8d01
Jean-Baptiste Brédif <Bredif.JBaptiste@gmail.com>
2022-03-15 21:11:25 +0100

    ajout page 404

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,9 +1,14 @@
 <template lang="fr">
-  <div>
-    <h1>404 ERROR NOT FOUND</h1>
+  <div class="text-center">
+    <img src="../assets/404_animation.gif" />
+    <router-link to='/' class='font-bold bg-white sm:px-16 sm:py-6 px-8 py-4 block rounded-full shadow'> Retourner à la page d'accueil</router-link>
   </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang=""></style>
+<style scoped>
+#view-container {
+  overflow: hidden;
+}
+</style>
